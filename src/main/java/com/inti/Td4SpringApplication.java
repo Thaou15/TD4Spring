928a5fb9124ee155bb75614e26c478162b12cdee
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-15 09:05:50 +0100

    version 1

---
+++ src/main/java/com/inti/Td4SpringApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td4SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td4SpringApplication.class, args);
+	}
+
+}
