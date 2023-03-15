742049b66ea47d24221cefea7ae03dd1faf90766
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-15 09:49:49 +0100

    version 2

--- src/main/java/com/inti/controller/MainController.java
+++ src/main/java/com/inti/controller/MainController.java
@@ -2,9 +2,13 @@ package com.inti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.inti.model.Voiture;
 import com.inti.repository.IVoitureRepository;
@@ -25,6 +29,30 @@ public class MainController {
 		 ivr.save(v);
 			
 		return "redirect:/ajout";
+	}
+	
+	@GetMapping("listeV")
+	public String listeVoiture(Model m)
+	{
+	    m.addAttribute("listeV",ivr.findAll());
+		return"listeV";
+	}
+	
+	@GetMapping("getVoiture")
+	public String getVoiture(@RequestParam("id") int id, Model m)
+	{
+		Voiture v = ivr.findById(id).get();
+		
+		m.addAttribute("voiture", v);
+		return"afficherVoiture";
+	}
+	
+	@GetMapping("delete/{id}")
+	public String deleteVoiture(@PathVariable int id)
+	{
+		ivr.deleteById(id);
+		
+		return "listeV";
 	}
 
 }
